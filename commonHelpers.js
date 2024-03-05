9e6c0fa2c34ed86799c75146efee635206840478
kiianskii <151513871+kiianskii@users.noreply.github.com>
2024-03-05 11:40:13 +0000

    Deploying to gh-pages from @ kiianskii/goit-js-hw-10@10a1ed23c1514ea4bfd52be534e1df273e2526e3 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as t}from"./assets/vendor-4daf66c6.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}},n=document.querySelector("input#datetime-picker");t(n,o);
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as e}from"./assets/vendor-4daf66c6.js";const o=document.querySelector("input#datetime-picker");document.querySelector("button[data-start]");const n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){console.log(t[0])}};e(o,n);
 //# sourceMappingURL=commonHelpers.js.map
