10a1ed23c1514ea4bfd52be534e1df273e2526e3
kiianskii <ankiyanskiy@gmail.com>
2024-03-05 13:39:54 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,16 +3,20 @@ import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const inputEl = document.querySelector("input#datetime-picker")
+const startBtn = document.querySelector("button[data-start]")
+
+let userSelectedDate;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+      console.log(selectedDates[0]);
   },
 };
 
-const inputEl = document.querySelector("input#datetime-picker")
-
 flatpickr(inputEl, options)
+
